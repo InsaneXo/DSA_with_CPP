e2bd000cf8e3995be7ee5aa59d7d084ccbcb8d59
InsaneXo <vsinghvipin333@gmail.com>
2025-08-10 00:37:15 +0530

    Today i solved 2 Question

---
+++ Day 2/Question6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    int row = 1;
+    int count = 1;
+
+    cout << "Enter your digits : ";
+    cin >> number;
+
+    while (row <= number)
+    {
+        int col = 1;
+
+        while (col <= row)
+        {
+            cout << count << " ";
+            count++;
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+}
